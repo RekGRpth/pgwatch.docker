48e4cf43ebe7d7825a9041825ba1326623f020e6
RekGRpth <rekgrpth@gmail.com>
2021-09-30 14:44:14 +0500

    up

--- run.sh
+++ run.sh
@@ -6,20 +6,20 @@ docker network create --attachable --opt com.docker.network.bridge.name=docker d
 docker stop pgwatch || echo $?
 docker rm pgwatch || echo $?
 docker run \
+    --detach \
     --env GROUP_ID="$(id -g)" \
     --env LANG=ru_RU.UTF-8 \
-    --env PGDATABASE=pgwatch \
-    --env PGHOST=/run/postgresql \
-    --env PGUSER=pgwatch \
+    --env PW2_DATASTORE=postgres \
+    --env PW2_PGDATABASE=pgwatch \
+    --env PW2_PGHOST=/run/postgresql \
+    --env PW2_PGUSER=pgwatch \
     --env TZ=Asia/Yekaterinburg \
     --env USER_ID="$(id -u)" \
     --hostname pgwatch \
-    --interactive \
     --mount type=bind,source=/etc/certs,destination=/etc/certs,readonly \
-    --mount type=volume,source=pgwatch,destination=/home \
     --mount type=bind,source=/run/postgresql,destination=/run/postgresql \
+    --mount type=volume,source=pgwatch,destination=/home \
     --name pgwatch \
     --network name=docker \
-    --rm \
-    --tty \
+    --restart always \
     ghcr.io/rekgrpth/pgwatch.docker su-exec pgwatch:pgwatch pgwatch2
